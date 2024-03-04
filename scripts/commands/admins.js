f19a2b35ce37fdd4acbbc5937446c562b229763b
Devilling1111 <157110204+Devilling1111@users.noreply.github.com>
2024-03-04 10:57:04 +0600

    Update admins.js

--- scripts/commands/admins.js
+++ scripts/commands/admins.js
@@ -1,32 +1,47 @@
 module.exports.config = {
-    name: 'admins',
-    version: '1.0.0',
+    name: "admin",
+    version: "1.0.0",
     permission: 0,
-    credits: 'nayan',
-    prefix: false,
-    description: 'group administrator list.',
-    category: 'without prefix',
-    usages: 'admins',
+    credits: "BADOL-KHAN",
+    prefix: true,
+    description: "",
+    category: "prefix",
+    usages: "",
     cooldowns: 5,
-    dependencies: []
+    dependencies: 
+	{
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
 };
-
-module.exports.run = async function({ api, event, args, Users }) {
-    var threadInfo = await api.getThreadInfo(event.threadID);
-    let qtv = threadInfo.adminIDs.length;
-    var listad = '';
-    var qtv2 = threadInfo.adminIDs;
-    var fs = global.nodemodule["fs-extra"];
-    dem = 1;
-    for (let i = 0; i < qtv2.length; i++) {
-        const info = (await api.getUserInfo(qtv2[i].id));
-        const name = info[qtv2[i].id].name;
-        listad += '' + `${dem++}` + '. ' + name + '\n';
-    }
-
-    api.sendMessage(
-        `list of ${qtv} administrators includes :\n${listad}`,
-        event.threadID,
-        event.messageID
-    );
-};
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+const time = process.uptime(),
+		hours = Math.floor(time / (60 * 60)),
+		minutes = Math.floor((time % (60 * 60)) / 60),
+		seconds = Math.floor(time % 60);
+const moment = require("moment-timezone");
+var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
+  
+var callback = () => api.sendMessage({body:`
+--------------------------------------------
+𝐍𝐚𝐦𝐞       : 𝐑𝐚𝐣𝐚 𝐁𝐚𝐛𝐮
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : 𝐑𝐚𝐣𝐚_𝐁𝐚𝐛𝐮
+𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
+𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐊𝐡𝐮𝐥𝐧𝐚, 𝐩𝐚𝐢𝐤𝐠𝐚𝐜𝐡𝐚
+𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐤𝐡𝐮𝐥𝐧𝐚, 𝐏𝐚𝐢𝐤𝐠𝐚𝐜𝐡𝐚 
+𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
+𝐀𝐠𝐞           : 𝟏𝟖+
+𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
+𝐖𝐨𝐫𝐤        : 𝐉𝐨𝐛
+𝐆𝐦𝐚𝐢𝐥       : rajababu420@@gmail.com
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/01782721761
+𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/BADOL_VAI_TERA_REAL_ABBU_OK
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/RAJA.BABU.TERA.REAL.ABBU.OK.07`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+    fs.unlinkSync(__dirname + "/cache/1.png"));  
+      return request(encodeURI(`https://graph.facebook.com/100007070042228/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
+   };
