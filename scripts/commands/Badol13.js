3409e9cfdd62c65776882075e59dc8f27f3bf5b2
Devilling1111 <157110204+Devilling1111@users.noreply.github.com>
2024-03-04 14:11:23 +0600

    Create Badol13.js

---
+++ scripts/commands/Badol13.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+module.exports.config = {
+	    name: "🙃",
+    version: "1.1.0",
+    permission: 0,
+    credits: "BADOL-KHAN",
+    description: "noprefix",
+    prefix: true,
+    category: "commands",
+    usages: "🙃",
+    cooldowns: 5,
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("🙂")==0 || event.body.indexOf("🙃")==0 || event.body.indexOf("🥴")==0 || event.body.indexOf("😒")==0) {
+		var msg = {
+				body: "ꗥ̳̳̳̳̳̳̳̳̳̳̿̿̿̿̿̿̿̿̿̿⃟ꗥꔸ𝐁𝐨𝐭-𝐎𝐰𝐧𝐞𝐫-𝐑𝐚𝐣𝐚-𝐁𝐚𝐛𝐮ꔸꗥ⃟ꗥ̳̳̳̳̳̳̳̳̳̳̿̿̿̿̿̿̿̿̿̿",
+				attachment: fs.createReadStream(__dirname + `/BADOL-KHAN/sentikawkno.mp3`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🤦‍♂️", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+          }
